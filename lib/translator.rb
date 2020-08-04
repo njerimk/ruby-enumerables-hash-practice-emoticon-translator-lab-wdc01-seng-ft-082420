c3cd4460e82ca40085f552b95b12ec92ee5919ee
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-08-04 15:06:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,9 +34,8 @@ def get_japanese_emoticon(file_path, emoticon)
 def get_english_meaning(file_path, emoticon)
   # code goes here
   data = load_library(file_path)
-  data.each do |name, value|
-    name.tap do |english, japanese|
-      puts japanese
+  data.select do |name, value|
+    puts emoticon == name
     end
   end
 end
