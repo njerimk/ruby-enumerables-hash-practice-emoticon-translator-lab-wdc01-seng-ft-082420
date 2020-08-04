df8d6b7ff12ecb4018c79e9881e92f9075624da0
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-08-04 15:07:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -35,7 +35,7 @@ def get_english_meaning(file_path, emoticon)
   # code goes here
   data = load_library(file_path)
   data.select do |name, value|
-    puts value[value]
+    puts value[name]
   end
 end
 
